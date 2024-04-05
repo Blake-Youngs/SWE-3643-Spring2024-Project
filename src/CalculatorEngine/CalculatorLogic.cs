58678826bb0c6739be81990da620fbd3e36bccf0
Blake-Youngs <blakeyoungs02@gmail.com>
2024-04-05 01:00:35 -0400

    Format Changes and Unit Tests methods made but empty

--- src/CalculatorEngine/CalculatorLogic.cs
+++ src/CalculatorEngine/CalculatorLogic.cs
@@ -1,84 +1,106 @@
 ﻿namespace CalculatorEngine;
 
-public partial class CalculatorLogic(int precision = 8) : ICalculatorLogic {
-    // Level of precision when comparing two floating point numbers
+// CalculatorLogic covers all calculation logic related requirements.
+public partial class CalculatorLogic(int precision = 8) : ICalculatorLogic
+{
+    // Level of precision when comparing two floating point numbers.
     private readonly int _precision = precision;
-     
-    public double Addition(double inputA, double inputB) {
+
+    public double Addition(double inputA, double inputB)
+    {
         // Preq-ENGINE-3
         return inputA + inputB;
     }
 
-    public double Subtraction(double inputA, double inputB) {
+    public double Subtraction(double inputA, double inputB)
+    {
         // Preq-ENGINE-4
         return inputA - inputB;
     }
 
-    public double Multiplication(double inputA, double inputB) {
+    public double Multiplication(double inputA, double inputB)
+    {
         // Preq-ENGINE-5
         return inputA * inputB;
     }
 
-    public double Division(double inputA, double inputB) {
+    public double Division(double inputA, double inputB)
+    {
         // Preq-ENGINE-7
-        if (inputB == 0) throw new DivideByZeroException();
+        if (inputB == 0)
+            throw new DivideByZeroException(); // Can't divide by 0.
 
         return inputA / inputB;
     }
-    
-    public bool Equals(double inputA, double inputB) {
+
+    public bool Equals(double inputA, double inputB)
+    {
         // Preq-ENGINE-8
+        // Compares both floating point numbers to a level of precision.
         var tolerance = Math.Pow(10, -_precision);
         var absoluteValueOfDiff = Math.Abs(inputA - inputB);
         var isWithinTolerance = absoluteValueOfDiff <= tolerance;
         return isWithinTolerance;
     }
 
-    public double RaiseToPower(double inputA, double inputB) {
+    public double RaiseToPower(double inputA, double inputB)
+    {
         // Preq-ENGINE-9
         return Math.Pow(inputA, inputB);
     }
 
-    public double Logarithm(double inputA, double inputB) {
+    public double Logarithm(double inputA, double inputB)
+    {
         // Preq-ENGINE-10
-        if (inputA <= 1) throw new ArgumentException("Input A must be greater than 1");
-        if (inputB <= 0) throw new Exception("Input B must be greater than 0");
+        if (inputA <= 1)
+            throw new ArgumentException(inputA + " must be greater than 1.");
+        if (inputB <= 0)
+            throw new Exception(inputB + " must be greater than 0.");
 
         return Math.Log(inputA, inputB);
     }
 
-    public double Root(double inputA, double inputB) {
+    public double Root(double inputA, double inputB)
+    {
         // Preq-ENGINE-11
-        if (inputB == 0) throw new ArgumentException("Input B cannot be 0");
+        if (inputB == 0)
+            throw new ArgumentException(inputB + " cannot be 0.");
 
         return Math.Pow(inputA, 1 / inputB);
     }
 
-    public double Factorial(double inputA) {
+    public double Factorial(double inputA)
+    {
         // Preq-ENGINE-12
         double result = 1;
-        for (var i = 0; i < inputA; i++) {
+        for (var i = 0; i < inputA; i++)
+        {
             result *= i;
         }
 
         return result;
     }
-    
-    public double Sine(double inputA) {
+
+    public double Sine(double inputA)
+    {
         return Math.Sin(inputA);
     }
 
-    public double Cosine(double inputA) {
+    public double Cosine(double inputA)
+    {
         return Math.Cos(inputA);
     }
 
-    public double Tangent(double inputA) {
+    public double Tangent(double inputA)
+    {
         return Math.Tan(inputA);
     }
-    
-    public double Reciprocal(double inputA) {
+
+    public double Reciprocal(double inputA)
+    {
         // Preq-ENGINE-16
-        if (inputA == 0) throw new ArgumentException("Input A cannot be 0");
+        if (inputA == 0)
+            throw new DivideByZeroException(inputA + " is cannot be used.");
 
         return 1 / inputA;
     }
