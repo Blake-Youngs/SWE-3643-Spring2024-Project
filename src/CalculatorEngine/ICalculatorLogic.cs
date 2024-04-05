f24bc84b7b7bd8f578a47fe111fa9ee561bf28d0
Blake-Youngs <blakeyoungs02@gmail.com>
2024-04-04 22:25:57 -0400

    Finished Calculator operations

--- src/CalculatorEngine/ICalculatorLogic.cs
+++ src/CalculatorEngine/ICalculatorLogic.cs
@@ -2,19 +2,24 @@
 
 public interface ICalculatorLogic
 {
-    public double Add(double inputA, double inputB); // A + B
+    // Arithmetic Operations
+    public double Addition(double inputA, double inputB); // A + B
     public double Subtraction(double inputA, double inputB); // A - B
     public double Multiplication(double inputA, double inputB); // A * B
     public double Division(double inputA, double inputB); // A / B
-    public double Equals(double inputA, double inputB); // A == B
     public double RaiseToPower(double inputA, double inputB); // A ^ B
     public double Logarithm(double inputA, double inputB); // A log B
     public double Root(double inputA, double inputB); // A root B
     
-    // Single input operations
+    
+    // Trigonometric Operations
     public double Factorial(double inputA); // A !
     public double Sine(double inputA); // sin A
     public double Cosine(double inputA); // cos A
     public double Tangent(double inputA); // tan A
+    
+    
+    // Miscellaneous Operations
+    public bool Equals(double inputA, double inputB); // A == B
     public double Reciprocal(double inputA); // 1 / A
 }
---
+++ src/CalculatorEngine/MiscellaneousOperations.cs
@@ -0,0 +1,19 @@
+﻿namespace CalculatorEngine;
+
+public partial class CalculatorLogic
+{
+    public bool Equals(double inputA, double inputB)
+    {
+        var tolerance = Math.Pow(10, -_precision);
+        var absoluteValueOfDiff = Math.Abs(inputA - inputB);
+        var isWithinTolerance = absoluteValueOfDiff <= tolerance;
+        return isWithinTolerance;
+    }
+    
+    public double Reciprocal(double inputA)
+    {
+        if (inputA == 0) throw new ArgumentException("Input A cannot be 0");
+
+        return 1 / inputA;
+    }
+}
